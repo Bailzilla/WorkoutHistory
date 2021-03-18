98abf4690f9348c343b50c3e7f0b2664a7950efc
Keith Bailey <bailey.keith.allen@gmail.com>
2021-03-18 12:52:34 -0400

    adding asyncHandler + POST and views

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const Workout = require('../models').Workout;
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
+
+
+/* POST create Workout. */
+router.post('/', asyncHandler(async (req, res) => {
+  let workout;
+  try {
+    workout = await Workout.create(req.body);
+    res.render('index', { title:'Post Test', name: req.body.username })
+  } catch (error) {
+    if(error.name === "SequelizeValidationError") {
+      workout = await Workout.build(req.body);
+      res.render("workouts/new", { workout, errors: error.errors, title: "New Workout" })
+    } else {
+      throw error;
+    }  
+  }
+}));
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
