f5fea2536daea3de5149d88f5db9a1cc92a4e70e
Keith Bailey <bailey.keith.allen@gmail.com>
2021-03-29 20:01:32 -0400

    experimenting with supertest

--- test/mainTest.js
+++ test/mainTest.js
@@ -1,7 +1,12 @@
-var expect = require('chai').expect;
+const assert = require('assert');
+const request = require("supertest");
+const app = require('../app');
 
-describe('Mocha', ()=>{
-    it('should run tests using NPM', ()=> {
-        expect(true).to.be.ok;
-    });
-});
+describe('Routes: workouts', () => {
+    describe('#GET /workouts', () => {
+        it('should redirect to a list of workouts', () => {
+            return request(app).get('/workouts')
+            .then((res) => {
+                assert(res.status, 302);
+                assert.match(res.header.location, /\/workouts\/\d/);
+ 
